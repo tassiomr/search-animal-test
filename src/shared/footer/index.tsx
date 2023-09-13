64cf744ef3308edd8065241631f0dd83cb91e93d
Tassio Rocha <tassiomarcosr@outlook.com>
2023-09-13 20:33:16 -0300

    style: finished footer component

--- src/shared/footer/index.tsx
+++ src/shared/footer/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Container } from './styles';
 import { Text } from '../../components/text';
+import { textColors, typography } from '../../theme';
 
 export const Footer = () => {
   return (
     <Container>
-      <Text text="ⓒ Google Inc" />
-      <Text text="version: 0.1.0" />
+      <Text text="ⓒ Google Inc" styles={{ fontSize: typography.verySmallText, color: textColors.footer }} />
+      <Text text="version: 0.1.0" styles={{ fontSize: typography.verySmallText, color: textColors.footer }} />
     </Container>
   );
 };
