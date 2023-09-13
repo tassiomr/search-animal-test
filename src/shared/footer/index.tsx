b62aec550b00d536c9e2d21399400cadc795da94
Tassio Rocha <tassiomarcosr@outlook.com>
2023-09-13 13:25:45 -0300

    style: finish footer component

---
+++ src/shared/footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container, Text } from './styles';
+
+export const Footer = () => {
+  return (
+    <Container>
+      <Text>ⓒ Google Inc</Text>
+      <Text>version: 0.1.0</Text>
+    </Container>
+  );
+};
---
+++ src/shared/footer/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex: 1;
+  width: 100%;
+  max-height: 70px;
+  background-color: #e3e6e8;
+  justify-content: space-between;
+`;
+
+export const Text = styled.p`
+  max-width: 120px;
+  flex: 1;
+  font-size: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #70757a;
+`;
