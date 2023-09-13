485da34665f1bf8ad087068b84edb9408d773fa2
Tassio Rocha <tassiomarcosr@outlook.com>
2023-09-13 14:31:51 -0300

    style: add icon on header componetn

--- src/shared/header/index.tsx
+++ src/shared/header/index.tsx
@@ -1,14 +1,18 @@
 export {};
 import React from 'react';
-import { Container } from './styles';
+import { Container, AvatarIconContainer } from './styles';
 import { Text } from '../../components/text';
 import { Avatar } from '../../components/avatar';
+import { MdApps } from 'react-icons/md';
 
 export const Header = () => {
   return (
     <Container>
       <Text text="AgileContent FrontEnd Test" />
-      <Avatar url={'https://i.scdn.co/image/ab6761610000e5eb4a61c7615c8125e832ffc32c'} />
+      <AvatarIconContainer>
+        <MdApps size={35} />
+        <Avatar url={'https://i.scdn.co/image/ab6761610000e5eb4a61c7615c8125e832ffc32c'} />
+      </AvatarIconContainer>
     </Container>
   );
 };
