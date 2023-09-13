435d4e0d775d689e62a72156559170a44abedfbb
Tassio Rocha <tassiomarcosr@outlook.com>
2023-09-13 14:32:36 -0300

    style: change padding on search component

--- src/shared/search-component/styles.ts
+++ src/shared/search-component/styles.ts
@@ -2,11 +2,11 @@ import styled from 'styled-components';
 
 export const InputContainer = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
   border: 1px solid #dfe1e5;
   border-radius: 24px;
-  padding: 10px;
+  padding: 12px;
   width: 100%;
   margin: 32px 0;
 `;
