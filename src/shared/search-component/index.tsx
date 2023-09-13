51c56751a4b478720326eab4431b8c1c675cd226
Tassio Rocha <tassiomarcosr@outlook.com>
2023-09-13 14:32:07 -0300

    style: add icons on search component

--- src/shared/search-component/index.tsx
+++ src/shared/search-component/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Container, InputContainer } from './styles';
 import { Input } from '../../components/input';
 import { Button } from '../../components/button';
+import { MdSearch, MdClose } from 'react-icons/md';
 
 export const SearchComponent = () => {
   return (
     <Container>
       <InputContainer>
+        <MdSearch size={25} />
         <Input />
+        <MdClose size={25} />
       </InputContainer>
       <Button />
     </Container>
