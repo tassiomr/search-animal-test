79e916783a4fe12f044664ac739050717724429e
Tassio Rocha <tassiomarcosr@outlook.com>
2023-09-16 17:15:54 -0300

    feat: cypress configs

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,19 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      require('@cypress/code-coverage/task')(on, config);
+
+      return config;
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: 'create-react-app',
+      bundler: 'webpack',
+    },
   },
 });
---
+++ cypress/plugins/index.js
@@ -0,0 +1,6 @@
+module.export = (on, config) => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('@cypress/code-coverage/task')(on, config);
+
+  return config;
+};
