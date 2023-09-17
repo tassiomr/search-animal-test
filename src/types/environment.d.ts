4efafe6838d02e6222a17c391e5b2acd75a66f02
Tassio Rocha <tassiomarcosr@outlook.com>
2023-09-17 14:14:16 -0300

    refactor: destination folder of type and remove mock

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -2,7 +2,7 @@ export declare global {
   export namespace NodeJS {
     export interface ProcessEnv {
       NODE_ENV: 'development' | 'production';
-      npm_package_version: string;
+      APP_VERSION: string;
     }
   }
 }
